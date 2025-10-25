03f8b902b217a74104fa00fbd57278f4bd2473bb
Yuliia Zhorniak bc2025-4 <YuliiaZhorniak.FEI@lnu.edu.ua>
2025-10-25 16:13:48 +0300

    main

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+const http = require('http');
 const { program } = require('commander');
 
 program
@@ -7,7 +9,20 @@ program
 
 program.parse(process.argv);
 const options = program.opts();
+
+const inputPath = options.input;
+const host = options.host;
+const port = Number(options.port);
+
 if (!fs.existsSync(inputPath)) {
   console.error('Cannot find input file');
   process.exit(1);
 }
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
+  res.end('Server is running');
+});
+
+server.listen(port, host, () => {
+  console.log(`Server listening on ${host}:${port}`);
+});
