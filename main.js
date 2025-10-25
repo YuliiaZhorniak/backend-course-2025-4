7845e7e7598a578b8c773fb6b7fa697418c4b44d
Yuliia Zhorniak bc2025-4 <YuliiaZhorniak.FEI@lnu.edu.ua>
2025-10-25 15:44:51 +0300

    main

---
+++ main.js
@@ -0,0 +1,9 @@
+const { program } = require('commander');
+
+program
+  .requiredOption('-i, --input <path>', 'path to input JSON file')
+  .requiredOption('-h, --host <host>', 'server host')
+  .requiredOption('-p, --port <port>', 'server port');
+
+program.parse(process.argv);
+const options = program.opts();
